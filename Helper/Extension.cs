e23dbac05a842d4fd24faf849f208b688fbbcf3e
ssz\nkumar326 <narender.kumar@dxc.com>
2020-09-09 21:04:34 +0530

    Add project files.

---
+++ Helper/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/// <summary>
+/// Helper is to assist with the Extension method for providing common functionality
+/// </summary>
+namespace Helper
+{
+    /// <summary>
+    /// Extension method for Convert operation
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// converts object to boolean
+        /// </summary>
+        /// <param name="value">expects object value</param>
+        /// <returns>returns either true/ false</returns>
+        public static bool ConvertToBoolean(this object value) => Convert.ToBoolean(value);
+    }
+}

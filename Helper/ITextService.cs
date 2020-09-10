7a2637834fc70c1c73be088c8bf50dfa27cd5ea7
Narender,K <help@websvc.in>
2020-09-10 14:48:37 +0530

    Added Text Services

---
+++ Helper/ITextService.cs
@@ -0,0 +1,8 @@
+﻿namespace Helper
+{
+    public interface ITextService
+    {
+        string ConvertToWords(int value);
+        string ConvertToNumbers(string value);
+    }
+}
---
+++ Helper/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    public class Service : ITextService
+    {
+        public string ConvertToNumbers(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToWords(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

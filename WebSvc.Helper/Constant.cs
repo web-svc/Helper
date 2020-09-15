c8bcaa37053506bd6cada8fdb7335728dd4c7149
Narender,K <help@websvc.in>
2020-09-15 21:06:41 +0530

    Renamed name space from Helper to WebSvc.Helper

---
+++ WebSvc.Helper/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace Helper
+{
+    internal static class Constant
+    {
+        internal const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        internal const string Numbers = "0123456789";
+        internal const string Expression_Alpha_Numeric = "[^a-zA-Z0-9_]+";
+    }
+}
